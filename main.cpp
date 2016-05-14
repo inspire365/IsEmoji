973ddfa606c2df044a51a7247bfdc7446aaf95c5
inspire365 <365days.enjoy@gmail.com>
2016-05-14 21:14:28 +0800

    other tables

--- main.cpp
+++ main.cpp
@@ -164,8 +164,81 @@ bool IsEmoji(const char* ch, uint32_t len, uint32_t* pos)
   if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x8E && ( (buf[3] >= 0x80 && buf[3] <= 0x93) || (buf[3] >= 0xA0 && buf[3] <= 0xBF) ))
     return true;
 
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x8F && ( (buf[3] >= 0x80 && buf[3] <= 0x84) || buf[3] == 0x86 || buf[3] == 0x88 || buf[3] == 0x8A))
+    return true;
 
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x8F && ( (buf[3] >= 0xA0 && buf[3] <= 0xA3) || (buf[3] >= 0xA5 && buf[3] <= 0xB0) ))
+    return true;
 
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x90 && (buf[3] == 0x8C || buf[3] == 0x8D || buf[3] == 0x8E || buf[3] == 0x91 || \
+     buf[3] == 0x92 || buf[3] == 0x94 || (buf[3] >= 0x97 && buf[3] <= 0xBE) ))
+    return true;
+
+  if(len > 3 && buf[0] ==0xF0 && buf[1] == 0x9F && buf[2] == 0x91 && (buf[3] == 0x80 || (buf[3] >= 0x82 && buf[3] <= 0xA4) || (buf[3] >= 0xA6 && buf[3] <= 0xAB) || \
+     buf[3] == 0xAE || buf[3] == 0xAF || (buf[3] >= 0xB0  && buf[3] <= 0xBF)) )
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x92 && ( (buf[3] >= 0x80 && buf[3] <= 0xAC) || (buf[3] >= 0xAE && buf[3] <= 0xB5) || \
+     (buf[3] >= 0xB8 && buf[3] <= 0xBF) ) )
+    return true;
+
+  if(len > 3 && buf[0] ==0xF0 && buf[1] == 0x9F && buf[2] == 0x93 && ( (buf[3] >= 0x80 && buf[3] <= 0xAB) || buf[3] == 0xAE || (buf[3] >= 0xB0 && buf[3] ==0xB4) || \
+     buf[3] == 0xB6 || buf[3] == 0xB7 || buf[3] == 0xB9 || buf[3] == 0xBA || buf[3] == 0xBB || buf[3] == 0xBC) )
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x94 && ( buf[3] == 0x83 || (buf[3] >= 0x8A && buf[3] <= 0x94) || (buf[3] >= 0x96 && buf[3] <= 0xAB) || \
+     (buf[3] >= 0xAE && buf[3] <= 0xBD) ))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] ==0x9F && buf[2] == 0x95 && (buf[3] >= 0x90 && buf[3] <= 0xBF))
+    return true;
+
+  // table 6a
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] ==0x98 && (buf[3] == 0x80 || buf[3] == 0x87 || buf[3] == 0x88 || buf[3] == 0x8E || buf[3] == 0x90 || \
+     buf[3] == 0x91 || buf[3] == 0x95 || buf[3] == 0x97 || buf[3] == 0x99 || buf[3] == 0x9B || buf[3] == 0x9F || buf[3] == 0xA6 || buf[3] == 0xA7 || \
+     buf[3] == 0xAC || buf[3] == 0xAE || buf[3] == 0xAF || buf[3] == 0xB4 || buf[3] == 0xB6))
+    return true;
+
+  // table 6b
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] ==0x9A && (buf[3] == 0x81 || buf[3] == 0x82 || buf[3] == 0x86 || buf[3] == 0x88 || buf[3] == 0x8A || \
+     buf[3] == 0x8D || buf[3] == 0x8E || buf[3] == 0x90 || buf[3] == 0x94 || buf[3] == 0x96 || buf[3] == 0x98 || (buf[3] >= 0x9B && buf[3] <= 0x9F) || \
+     buf[3] == 0xA0 || buf[3] == 0xA1 || buf[3] == 0xA3 || buf[3] == 0xA6 || buf[3] == 0xAE || buf[3] == 0xAF || buf[3] == 0xB0 || buf[3] ==0xB1 || \
+     buf[3] == 0xB3 || buf[3] == 0xB4 || buf[3] == 0xB5 || buf[3] == 0xB7 || buf[3] == 0xB8 || buf[3] == 0xBF))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x9B && (buf[3] >= 0x81 && buf[3] <= 0x85))
+    return true;
+
+  // table 6c
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x8C && (buf[3] == 0x8D || buf[3] == 0x8E || buf[3] == 0x90 || buf[3] == 0x92 || buf[3] == 0x96 || \
+     buf[3] == 0x97 || buf[3] == 0x98 || buf[3] == 0x9A || buf[3] == 0x9C || buf[3] == 0x9D || buf[3] == 0x9E || buf[3] == 0xB2 || buf[3] == 0xB3))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x8D && (buf[3] == 0x8B || buf[3] == 0x90 || buf[3] == 0xBC))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x8F && (buf[3] == 0x87 || buf[3] == 0x89 || buf[3] == 0xA4))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x90 && ( (buf[3] >= 0x80 && buf[3] <= 0x8B) || buf[3] == 0x8F || buf[3] == 0x90 || \
+      buf[3] == 0x93 || buf[3] == 0x95 || buf[3] == 0x96))
+      return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x91 && (buf[3] == 0xA5 || buf[3] == 0xAC || buf[3] == 0xAD))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x92 && (buf[3] == 0xAD || buf[3] == 0xB6 || buf[3] == 0xB7))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x93 && (buf[3] == 0xAC || buf[3] == 0xAD || buf[3] == 0xAF || buf[3] == 0xB5))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x94 && ( (buf[3] >= 0x80 && buf[3] <= 0x82) || (buf[3] >= 0x84 && buf[3] <= 0x87) || \
+     buf[3] == 0x89 || buf[3] == 0x95 || buf[3] == 0xAC || buf[3] == 0xAC || buf[3] == 0xAD))
+    return true;
+
+  if(len > 3 && buf[0] == 0xF0 && buf[1] == 0x9F && buf[2] == 0x95 && (buf[3] >= 0x9C && buf[3] <= 0xA7))
+    return true;
 
   return false;
 
